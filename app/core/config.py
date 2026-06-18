2c326e56289be26d05414cd7c2a7bedff4bbeae9
Say <sainirgg@inbox.ru>
2026-06-19 02:43:12 +0600

    fix(variables): let aws settings be None

--- app/core/config.py
+++ app/core/config.py
@@ -20,11 +20,11 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
-    AWS_ACCESS_KEY_ID: str
-    AWS_SECRET_ACCESS_KEY: str
-    AWS_REGION_NAME: str
-    AWS_S3_BUCKET_NAME: str
-    AWS_S3_BASE_URL: str
+    AWS_ACCESS_KEY_ID: str | None = None
+    AWS_SECRET_ACCESS_KEY: str | None = None
+    AWS_REGION_NAME: str | None = None
+    AWS_S3_BUCKET_NAME: str | None = None
+    AWS_S3_BASE_URL: str | None = None
     AWS_PRESIGN_EXPIRES_SECONDS: int = 3600
     S3_ENDPOINT_URL: str | None = None
 
